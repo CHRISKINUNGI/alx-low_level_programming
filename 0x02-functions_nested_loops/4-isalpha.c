5477f742b45692a7c0413259cc695ac100ac12c8
CHRISKINUNGI <chrskinungi@gmail.com>
2023-03-22 23:29:00 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: checks the variable type
+ *
+ * Return: 1 c is alphabet
+ *         0 c is not alphabet:
+ *
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
