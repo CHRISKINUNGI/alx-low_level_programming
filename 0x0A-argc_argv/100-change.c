cb092d53ad0aec6d3d09fe7842b059499ce82b83
CHRISKINUNGI <chrskinungi@gmail.com>
2023-04-10 20:52:35 +0300

    commit to 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,54 @@
+#include"main.h"
+
+/**
+ * main - entry point of a program
+ *
+ * Description -  program that prints the minimum number of coins to make change for an amount of money.
+ *
+ * @argc: int variable for number of arguments
+ *
+ * @argv: array string for arguments
+ *
+ * Return: 1 Error, 0 successfull termination
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+       	int amountinCents, numberofCoins;
+
+	if (argc != 2)
+        {
+                printf("Error\n");
+                return (0);
+        }
+
+	numberofCoins = 0;
+	amountinCents = atoi(argv[1]);
+
+
+	if (amountinCents < 0)
+	{
+		printf("0\n");
+	}
+
+	else
+	{
+		numberofCoins += amountinCents / 25;
+		amountinCents %= 25;
+
+		numberofCoins += amountinCents / 10;
+		amountinCents %= 10;
+
+		numberofCoins += amountinCents / 5;
+		amountinCents %= 5;
+
+		numberofCoins += amountinCents / 2;
+		amountinCents %= 2;
+
+		numberofCoins += amountinCents / 1;
+
+		printf("%d\n", numberofCoins);
+	}
+	return (0);
+}
