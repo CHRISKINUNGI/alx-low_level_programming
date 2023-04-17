e880665dfd43e2271472127ca86b5fca5370962a
CHRISKINUNGI <chrskinungi@gmail.com>
2023-04-17 21:53:55 +0300

    commit to add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include "main.h"
+
+/**
+ * struct dog - holds the attributes of a dog
+ *
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: struct dog
+ *
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float *age;
+};
+
+#endif
