358dba4804dc372356af1884581bafd47ba6f290
CHRISKINUNGI <chrskinungi@gmail.com>
2023-04-17 22:24:53 +0300

    commit to add corrections

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - holds the attributes of a dog
@@ -14,11 +15,16 @@
  *
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
-	float *age;
-};
+	float age;
+}dog_t;
+
+ void init_dog(struct dog *d, char *name, float age, char *owner);
+ void print_dog(struct dog *d);
+ dog_t *new_dog(char *name, float age, char *owner);
+ void free_dog(dog_t *d);
 
 #endif
