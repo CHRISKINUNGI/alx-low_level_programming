329bcce5e52e8c1a5c55bba2afb0c5e0f45ea40b
CHRISKINUNGI <chrskinungi@gmail.com>
2023-04-19 16:21:06 +0300

    commit to add  1-array_iterator.c file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,20 +1,6 @@
 #include "function_pointers.h"
 
 /**
- * action - function to print elements of an array
- *
- * @functionaddress: contains the elements
- *
- * Return: nothing
- *
- */
-
-void action(int atoi(array_iterator))
-{
-	action = array_iterator;
-}
-
-/**
  * array_iterator - function to execute a function
  *
  * @array: array of functions
@@ -27,5 +13,15 @@ void action(int atoi(array_iterator))
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	action(array);
+	unsigned int counter;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+
+	for (counter = 0; counter < size; counter++)
+	{
+		action(array[counter]);
+	}
 }
