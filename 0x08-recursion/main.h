bd5a48e62918ee0d3bda228f7e13906e4b896aeb
CHRISKINUNGI <chrskinungi@gmail.com>
2023-05-01 03:33:11 +0300

    commit to correct 100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
 
-int _sqrt_finder(int n, int min, int max);
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -12,5 +14,10 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+
+
 
 #endif
