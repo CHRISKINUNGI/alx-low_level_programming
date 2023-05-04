66a4722d804391aebec2bef5323674edf972e8ab
CHRISKINUNGI <chrskinungi@gmail.com>
2023-05-04 23:35:47 +0300

    commit to add 1-print_binary.c and test file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_binary - print an int in binary
+ *
+ * @n: variable of the int being printed
+ *
+ * Return: void
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	int firstCounter, secondCounter;
+	unsigned long int binaryFormat;
+
+	secondCounter = 0;
+	firstCounter = 0;
+	binaryFormat = 0;
+
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+	
+	while (n > 0)
+	{
+		binaryFormat = n & 1;
+
+		if (binaryFormat == 0)
+			secondCounter = 1;
+		if (secondCounter == 1)
+			putchar(binaryFormat + '0');
+
+		n = n >> 1;
+		firstCounter++;	}
+}
